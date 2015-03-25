8093fc9b101b82d35d11373c056ce3043e6529da
oblitera <blanpain_georges@hotmail.fr>
2015-03-26 00:14:22 +0100

    ajout carte interactive

---
+++ guide/web/js/carte/france/buildemap_index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$('#francemap').vectorMap(
+		{
+		    map: 'france_fr',
+			hoverOpacity: 0.5,
+			hoverColor: "#EC0000",
+			backgroundColor: "#ffffff",
+			color: "#309ac1",
+			borderColor: "#000000",
+			selectedColor: "#EC0000",
+			onRegionOver: function(event, code, region)
+			{
+				$("[id_map="+code+"]").addClass("region_survoler");
+			},
+			onRegionOut: function(event, code, region)
+			{
+				$("[id_map="+code+"]").removeClass("region_survoler");
+			},
+		    onRegionClick: function(element, code, region)
+		    {
+		        var message = 'Région : "'
+		            + region 
+		            + '" || Id : "'
+		            + code
+					+ '"';
+             
+		        alert(message);
+		    }
+		}
+	);
+});
