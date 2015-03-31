7a16e87eaf637cc4c40a96fc2956b217cc6abc68
oblitera <blanpain_georges@hotmail.fr>
2015-03-31 22:16:44 +0200

    joujou avec redis

--- guide/src/GUI/AnnoncesBundle/Controller/AnnonceController.php
+++ guide/src/GUI/AnnoncesBundle/Controller/AnnonceController.php
@@ -3,6 +3,7 @@
 namespace GUI\AnnoncesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class AnnonceController extends Controller
 {
@@ -13,6 +14,14 @@ class AnnonceController extends Controller
 
     public function voirAction()
     {
-        return $this->render('GUIAnnoncesBundle:Annonce:voir.html.twig');    	
+    	$redis = $this->get('snc_redis.default');
+    	$cache = $redis->get('voir');
+    	return new Response($cache); 
+
+    	/*$reponse = $this->render('GUIAnnoncesBundle:Annonce:voir.html.twig');
+    	$redis = $this->get('snc_redis.default');
+    	$redis->set("voir",$reponse->getContent());*/
+
+        //return $this->render('GUIAnnoncesBundle:Annonce:voir.html.twig');    	
     }
 }
